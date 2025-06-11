6f1a8e308b89de7b9fce33d33fe834fa7c694324
josephdinesh024 <140788878+josephdinesh024@users.noreply.github.com>
2025-06-11 19:06:49 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
-from admin import app,db
+import os
+from admin import app, db
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT env variable
+    app.run(host="0.0.0.0", port=port, debug=False) 
